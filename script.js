d4ecc0f88eb6bc46feb1d3e4ce64c95aca106dad
Akin-Inceler <akin2001inceler@gmail.com>
2026-05-05 16:53:04 +0200

    add functionality to save input leads and display them in a list

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 
-let saveBtn = document.getElementById("save-btn")
+let myLeads = ["www.awesomelead.com"]
+const inputEl = document.getElementById("input-el")
+const saveBtn = document.getElementById("save-btn")
+const ulEl = document.getElementById("ul-el")
+
+
 saveBtn.addEventListener("click", function() {
-    console.log("Button clicked from event listener")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
